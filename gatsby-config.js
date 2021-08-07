37ec9932742c514b50f186f5c0faf62b66978130
Faraz Shaikh <farazzshaikh@gmail.com>
2021-08-07 16:04:05 +0400

    starter

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
       resolve: "gatsby-source-sanity",
       options: {
         projectId: "gxqfhjxx",
-        dataset: "",
+        dataset: "posts",
       },
     },
     "gatsby-plugin-sass",
